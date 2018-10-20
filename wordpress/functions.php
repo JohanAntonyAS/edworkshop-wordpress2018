19ed632257df0be4de96c4ecb5c780859f255270
jonmircha <jonmircha@gmail.com>
2018-10-19 19:22:02 -0500

    Agregando recomendaciones finales

---
+++ wordpress/functions.php
@@ -0,0 +1,40 @@
+<?php
+// Ocultar la versión de WP de las metatags
+remove_action( 'wp_head', 'wp_generator' );
+
+
+// Bloquear mensajes de errores en la entrada al administrador
+add_filter( 'login_errors', create_function('$a', 'return null;') );
+
+
+// Bloquear visitantes que usen Servidores Proxies
+function block_proxy_visits () {
+  if ( !is_user_logged_in() ) {
+    if ( @fsockopen( $_SERVER['REMOTE_ADDR'], 80, $errstr, $errno, 1 ) ) {
+      die( 'No se permite el acceso mediante Proxy' );
+    }
+  }
+}
+
+add_action('after_setup_theme', 'block_proxy_visits');
+
+
+// Evitar la enumeración de usuarios
+// http://example.com/?author=1
+function check_users_enum ( $redirect, $request ) {
+  if ( preg_match( '/\?author=([0-9]*)(\/*)/i', $request ) ) die();
+  else return $redirect;
+}
+
+if ( !is_admin() ) {
+  if ( preg_match( '/author=([0-9]*)/i', $_SERVER['QUERY_STRING'] ) ) die();
+  add_filter( 'redirect_canonical', 'check_users_enum', 10, 2);
+}
+
+// Crear nueva regla rewrite
+add_action( 'init', 'wp_ozh_plu_rewrite' );
+function wp_ozh_plu_rewrite() {
+    add_rewrite_rule( 'login/?$', 'wp-login.php', 'top' );
+}
+
+
---
+++ wordpress/wp-config.php
@@ -0,0 +1,30 @@
+<?php
+// Habilitar todas las actualizaciones básicas, incluyendo menores y mayores
+define( 'WP_AUTO_UPDATE_CORE', true );
+// Habilitar actualizaciones fundamentales para versiones menores (por defecto)
+define( 'WP_AUTO_UPDATE_CORE', 'minor' );
+
+
+// Forzar inicio de sesión seguro a la administración.
+define( 'FORCE_SSL_ADMIN', true );
+// Forzar inicio de sesión seguro.
+define( 'FORCE_SSL_LOGIN', true );
+
+
+// Mover el directorio wp-content
+define( 'WP_CONTENT_DIR', dirname(__FILE__) . '/assets' );
+define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/edwp-workshop/con-duplicator/assets' );
+
+
+// Mover el directorio plugin
+define( 'WP_PLUGIN_DIR', dirname(__FILE__) . '/assets/plugins' );
+define( 'WP_PLUGIN_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/edwp-workshop/con-duplicator/assets/plugins' );
+
+
+// Remplaza los permisos de archivos predeterminados en Wordpress
+define ( 'FS_CHMOD_DIR', ( 0755 & ~ umask () ) );
+define ( 'FS_CHMOD_FILE', ( 0644 & ~ umask () ) );
+
+
+
+
